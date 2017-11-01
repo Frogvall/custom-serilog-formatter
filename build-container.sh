008b6b2719f94c3f8a17aa262b5f133c6ea0c294
btovb8 <btovb8@RD0051975>
2017-11-01 10:31:58 +0100

    fixes for tests

--- build-container.sh
+++ build-container.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 set -e
 dotnet restore
-dotnet test test/custom-serilog-formatter.test/custom-serilog-formatter.test.csproj -xml $(pwd)/testresults/out.xml
+cd  test/custom-serilog-formatter.test
+dotnet xunit -xml $(pwd)/../../testresults/out.xml
+cd -
 rm -rf $(pwd)/package
 dotnet pack src/custom-serilog-formatter/custom-serilog-formatter.csproj -c release -o $(pwd)/package --version-suffix=${BuildNumber}
 mkdir $(pwd)/symbols
