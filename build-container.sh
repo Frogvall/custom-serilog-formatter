37a993c71de75760104a867f4223aaf41ca0e2fc
btovb8 <btovb8@RD0051975>
2017-11-01 13:24:51 +0100

    removed version suffix

--- build-container.sh
+++ build-container.sh
@@ -6,4 +6,4 @@ dotnet restore
 dotnet xunit -xml $(pwd)/../../testresults/out.xml
 cd -
 rm -rf $(pwd)/package
-dotnet pack src/custom-serilog-formatter/custom-serilog-formatter.csproj -c release -o $(pwd)/package --version-suffix=${BuildNumber}
+dotnet pack src/custom-serilog-formatter/custom-serilog-formatter.csproj -c release -o $(pwd)/package
