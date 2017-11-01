6c87e9fa73ba458a1fd461c8bf6cf0d2b4b1c98a
btovb8 <btovb8@RD0051975>
2017-11-01 10:22:11 +0100

    missed to update

--- build-container.sh
+++ build-container.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 set -e
 dotnet restore
-dotnet test test/custom-serilog-formatter.test/project.json -xml $(pwd)/testresults/out.xml
+dotnet test test/custom-serilog-formatter.test/custom-serilog-formatter.test.csproj -xml $(pwd)/testresults/out.xml
 rm -rf $(pwd)/package
-dotnet pack src/custom-serilog-formatter/project.json -c release -o $(pwd)/package --version-suffix=${BuildNumber}
+dotnet pack src/custom-serilog-formatter/custom-serilog-formatter.csproj -c release -o $(pwd)/package --version-suffix=${BuildNumber}
 mkdir $(pwd)/symbols
 cp $(pwd)/package/*.symbols.nupkg $(pwd)/symbols
 rm $(pwd)/package/*.symbols.nupkg
